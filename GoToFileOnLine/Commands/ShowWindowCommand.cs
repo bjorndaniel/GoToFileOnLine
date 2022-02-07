ac3a1f9c226325363ffe4bc6816b27733c4ea9b6
Daniel Sandberg <bjorndaniel@gmail.com>
2022-02-07 20:29:00 +0100

    First release to Beta-testers

--- GoToFileOnLine/Commands/ShowWindowCommand.cs
+++ GoToFileOnLine/Commands/ShowWindowCommand.cs
@@ -5,10 +5,11 @@ namespace GoToFileOnLine
     [Command(PackageIds.GoToFileOnLineCommand)]
     internal sealed class GoToFileOnLineCommand : BaseCommand<GoToFileOnLineCommand>
     {
+
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
-            var x = new GoToFileOnLineSearchWindow();
-            x.Show();
+            var searchWindow = new GoToFileOnLineSearchWindow();
+            var done = searchWindow.ShowDialog();
         }
     }
 }
--- GoToFileOnLine/ToolWindows/GoToFileOnLineSearchWindow.xaml.cs
+++ GoToFileOnLine/ToolWindows/GoToFileOnLineSearchWindow.xaml.cs
@@ -2,10 +2,12 @@
 using EnvDTE80;
 using GoToFileOnLine.Models;
 using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.TextManager.Interop;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -15,96 +17,154 @@ namespace GoToFileOnLine
 {
     public partial class GoToFileOnLineSearchWindow : DialogWindow
     {
-        private ObservableCollection<ComboBoxItemModel> ProjectItems = new ObservableCollection<ComboBoxItemModel>();
+        private List<ComboBoxItemModel> ProjectItems = new List<ComboBoxItemModel>();
         private ComboBoxItemModel _selectedItem;
+        private IVsOutputWindow _outputWindow;
+        private Guid _paneGuid = Guid.NewGuid();
+        public Brush _defaultTextColor;
 
         public GoToFileOnLineSearchWindow()
         {
             InitializeComponent();
-
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD100:Avoid async void methods", Justification = "<Pending>")]
-        protected override async void OnInitialized(EventArgs e)
+        protected override async void OnActivated(EventArgs e)
         {
-            base.OnInitialized(e);
-            var dte2 = (DTE2)Package.GetGlobalService(typeof(DTE));
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-            foreach (EnvDTE.Project proj in dte2.Solution.Projects)
-            {
-                foreach (ProjectItem item in proj.ProjectItems)
-                {
-                    if (item.Properties?.Item("FullPath")?.Value != null)
-                    {
-                        ProjectItems.Add(new ComboBoxItemModel
-                        {
-                            FullPath = item.Properties?.Item("FullPath")?.Value.ToString(),
-                            Name = item.Name
-                        });
-                    }
-                }
-            }
+            base.OnActivated(e);
+            await GetAllSolutionFilesAsync();
             SearchBox.Focus();
+            _defaultTextColor = SearchBox.Foreground;
         }
 
+        private async Task GetAllSolutionFilesAsync()
+        {
+            try
+            {
+                ProjectItems.Clear();
+                var dte2 = (DTE2)Package.GetGlobalService(typeof(DTE));
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                foreach (EnvDTE.Project project in dte2.Solution.Projects)
+                {
+                    ProjectItems.AddRange(await GetProjectFilesAsync(project.ProjectItems));
+                }
+                ProjectItems = ProjectItems.OrderBy(x => x.Name).ToList();
+            }
+            catch (Exception e)
+            {
+                await PrintToOutputAsync(e.ToString());
+            }
+        }
+
+        public async Task<IEnumerable<ComboBoxItemModel>> GetProjectFilesAsync(ProjectItems items)
+        {
+            var result = new List<ComboBoxItemModel>();
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            foreach (ProjectItem item in items)
+            {
+                if (item.Name?.Contains(".") ?? false && item.ProjectItems != null && item.Properties?.Item("FullPath")?.Value != null)
+                {
+                    var cb = new ComboBoxItemModel
+                    {
+                        Name = item.Name!,
+                        FullPath = item.Properties?.Item("FullPath")?.Value.ToString(),
+                    };
+
+                    if (!string.IsNullOrWhiteSpace(cb.FullPath))
+                    {
+                        result.Add(cb);
+                    }
+                }
+                if (item.ProjectItems != null)
+                {
+                    result.AddRange(await GetProjectFilesAsync(item.ProjectItems));
+                }
+                if (item.SubProject != null && item.SubProject.ProjectItems != null)
+                {
+                    result.AddRange(await GetProjectFilesAsync(item.SubProject.ProjectItems));
+                }
+            }
+            return result;
+        }
 
         //From https://www.c-sharpcorner.com/UploadFile/201fc1/autocomplete-textbox-in-wpf-using-only-net-and-wpf-librari/
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD100:Avoid async void methods", Justification = "<Pending>")]
         private async void TextBox_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Enter)
+            try
             {
-                if (_selectedItem != null)
+                if (e.Key == Key.Enter)
                 {
-                    await GoToFileAsync(_selectedItem);
+                    if (_selectedItem != null)
+                    {
+                        await GoToFileAsync(_selectedItem);
+                    }
                 }
-            }
-            if (e.Key == Key.Down)
-            {
-                if (_selectedItem != null)
+                if (e.Key == Key.Escape)
                 {
-                    ((TextBlock)ResultStack.Children[_selectedItem.Index]).Background = Brushes.Transparent;
-                    if (_selectedItem.Index >= ResultStack.Children.Count - 1)
+                    Close();
+                    return;
+                }
+                if (e.Key == Key.Down)
+                {
+                    if (_selectedItem != null)
+                    {
+                        foreach (var item in ResultStack.Children)
+                        {
+                            ((TextBlock)item).Background = Brushes.Transparent;
+                            ((TextBlock)item).Foreground = _defaultTextColor;
+                        }
+                        if (_selectedItem.Index >= ResultStack.Children.Count - 1)
+                        {
+                            _selectedItem = ((TextBlock)ResultStack.Children[0]).Tag as ComboBoxItemModel;
+                        }
+                        else
+                        {
+                            _selectedItem = ((TextBlock)ResultStack.Children[_selectedItem.Index + 1]).Tag as ComboBoxItemModel;
+                        }
+                        SetColors((TextBlock)ResultStack.Children[_selectedItem.Index]);
+                    }
+                    else if (ResultStack.Children.Count > 0)
                     {
                         _selectedItem = ((TextBlock)ResultStack.Children[0]).Tag as ComboBoxItemModel;
-                        ((TextBlock)ResultStack.Children[0]).Background = Brushes.DarkGray;
+                        SetColors((TextBlock)ResultStack.Children[_selectedItem.Index]);
                     }
-                    else
+                    return;
+                }
+                if (e.Key == Key.Up)
+                {
+                    if (_selectedItem != null)
                     {
-                        _selectedItem = ((TextBlock)ResultStack.Children[_selectedItem.Index + 1]).Tag as ComboBoxItemModel;
-                        ((TextBlock)ResultStack.Children[_selectedItem.Index]).Background = Brushes.DarkGray;
+                        ((TextBlock)ResultStack.Children[_selectedItem.Index]).Background = Brushes.Transparent;
+                        ((TextBlock)ResultStack.Children[_selectedItem.Index]).Foreground = _defaultTextColor;
+
+                        if (_selectedItem.Index < 1)
+                        {
+                            _selectedItem = ((TextBlock)ResultStack.Children[ResultStack.Children.Count - 1]).Tag as ComboBoxItemModel;
+                        }
+                        else
+                        {
+                            _selectedItem = ((TextBlock)ResultStack.Children[_selectedItem.Index - 1]).Tag as ComboBoxItemModel;
+                        }
+                        SetColors((TextBlock)ResultStack.Children[_selectedItem.Index]);
                     }
-                }
-                else
-                {
-                    _selectedItem = ((TextBlock)ResultStack.Children[0]).Tag as ComboBoxItemModel;
-                    ((TextBlock)ResultStack.Children[0]).Background = Brushes.DarkGray;
-                }
-                return;
-            }
-            if (e.Key == Key.Up)
-            {
-                if (_selectedItem != null)
-                {
-                    ((TextBlock)ResultStack.Children[_selectedItem.Index]).Background = Brushes.Transparent;
-                    if (_selectedItem.Index < 1)
+                    else if (ResultStack.Children.Count > 0)
                     {
                         _selectedItem = ((TextBlock)ResultStack.Children[ResultStack.Children.Count - 1]).Tag as ComboBoxItemModel;
-                        ((TextBlock)ResultStack.Children[_selectedItem.Index]).Background = Brushes.DarkGray;
+                        SetColors((TextBlock)ResultStack.Children[_selectedItem.Index]);
                     }
-                    else
-                    {
-                        _selectedItem = ((TextBlock)ResultStack.Children[_selectedItem.Index - 1]).Tag as ComboBoxItemModel;
-                        ((TextBlock)ResultStack.Children[_selectedItem.Index]).Background = Brushes.DarkGray;
-                    }
+                    return;
                 }
-                else
-                {
-                    _selectedItem = ((TextBlock)ResultStack.Children[ResultStack.Children.Count - 1]).Tag as ComboBoxItemModel;
-                    ((TextBlock)ResultStack.Children[_selectedItem.Index]).Background = Brushes.DarkGray;
-                }
-                return;
+                PerformQuery(sender);
             }
+            catch (Exception ex)
+            {
+                await PrintToOutputAsync(ex.ToString());
+            }
+        }
+
+        private void PerformQuery(object sender)
+        {
             var border = (ResultStack.Parent as ScrollViewer).Parent as Border;
             var query = (sender as TextBox).Text;
             query = query.Contains(":") ? query.Split(':').First() : query;
@@ -113,6 +173,7 @@ namespace GoToFileOnLine
                 // Clear
                 ResultStack.Children.Clear();
                 border.Visibility = Visibility.Collapsed;
+                return;
             }
             else
             {
@@ -128,24 +189,34 @@ namespace GoToFileOnLine
                 item.Index = counter;
                 AddItem(item);
                 counter++;
+                if (counter > 10)
+                {
+                    break;
+                }
             }
 
-            if (results.Count == 0)
+            if (ResultStack.Children.Count == 0)
             {
                 ResultStack.Children.Add(new TextBlock() { Text = "No results found." });
             }
-            else if (results.Count == 1)
+            else if (ResultStack.Children.Count == 1)
             {
                 _selectedItem = ((TextBlock)ResultStack.Children[0]).Tag as ComboBoxItemModel;
-                ((TextBlock)ResultStack.Children[0]).Background = Brushes.DarkGray;
+                SetColors((TextBlock)ResultStack.Children[_selectedItem.Index]);
             }
         }
 
+        private void SetColors(TextBlock tb)
+        {
+            tb.Background = Brushes.DarkGray;
+            tb.Foreground = Brushes.Black;
+        }
+
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "VSTHRD101:Avoid unsupported async delegates", Justification = "<Pending>")]
-        private async void AddItem(ComboBoxItemModel p)
+        private void AddItem(ComboBoxItemModel p)
         {
             var block = new TextBlock();
-
+            block.Foreground = _defaultTextColor;
             // Add the text
             block.Text = p.Name ?? string.Empty;
             block.Tag = p;
@@ -179,25 +250,50 @@ namespace GoToFileOnLine
 
         private async Task GoToFileAsync(ComboBoxItemModel cb)
         {
-            var lineText = SearchBox.Text.Contains(":") ? SearchBox.Text.Split(':').Last() : "1";
-            int.TryParse(lineText, out var lineNumber);
-            var path = cb.FullPath;
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-            var provider = (Microsoft.VisualStudio.OLE.Interop.IServiceProvider)Package.GetGlobalService(typeof(DTE));
-            VsShellUtilities.OpenDocument(new ServiceProvider(provider),
-                path, Guid.Empty,
-                out _,
-                out _,
-                out _,
-                out IVsTextView viewAdapter);
-            viewAdapter.CenterLines(lineNumber > 10 ? lineNumber - 10 : lineNumber, 20);
-            viewAdapter.SetCaretPos(lineNumber - 1, 0);
-            var lines = File.ReadAllLines(path);
-            if (lines.Count() > lineNumber)
+            try
             {
-                viewAdapter.SetSelection(lineNumber - 1, 0, lineNumber - 1, lines[lineNumber - 1].Length);
+                var lineText = SearchBox.Text.Contains(":") ? SearchBox.Text.Split(':').Last() : "1";
+                int.TryParse(lineText, out var lineNumber);
+                var path = cb.FullPath;
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                var provider = (Microsoft.VisualStudio.OLE.Interop.IServiceProvider)Package.GetGlobalService(typeof(DTE));
+                VsShellUtilities.OpenDocument(new ServiceProvider(provider),
+                    path, Guid.Empty,
+                    out _,
+                    out _,
+                    out _,
+                    out IVsTextView viewAdapter);
+                if (viewAdapter != null)
+                {
+                    viewAdapter.CenterLines(lineNumber > 10 ? lineNumber - 10 : lineNumber, 20);
+                    viewAdapter.SetCaretPos(lineNumber - 1, 0);
+                    var lines = File.ReadAllLines(path);
+                    if (lines.Count() > lineNumber)
+                    {
+                        viewAdapter.SetSelection(lineNumber - 1, 0, lineNumber - 1, lines[lineNumber - 1].Length);
+                    }
+                }
             }
+            catch (Exception e)
+            {
+                await PrintToOutputAsync(e.ToString());
+            }
+            SearchBox.Text = string.Empty;
             Close();
         }
+
+        private async Task PrintToOutputAsync(string text)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            if (_outputWindow == null)
+            {
+                _outputWindow = (IVsOutputWindow)Package.GetGlobalService(typeof(SVsOutputWindow));
+                _outputWindow.CreatePane(ref _paneGuid, "Go to file on line", 1, 1);
+            }
+            _outputWindow.GetPane(ref _paneGuid, out var outputPane);
+            outputPane.Activate();
+            outputPane.OutputString($"{text}{Environment.NewLine}");
+        }
+
     }
 }
